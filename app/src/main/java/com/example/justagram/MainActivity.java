a0bab504d31261b24701686f9f03bae7c2218cf7
TommyDatLCLapGaming <datsieusaiya12@gmail.com>
2025-09-29 22:08:25 +0700

    Update main activity

---
+++ app/src/main/java/com/example/justagram/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.justagram;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+}
