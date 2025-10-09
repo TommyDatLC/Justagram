477380cf48a29a32011ac8c08eec7e69a1d3186c
Nguyen Duc Anh <santademonZ123@gmail.com>
2025-10-09 16:29:54 +0700

    Updated for getting images and reel_videos of an account

---
+++ app/src/main/java/com/example/justagram/Post.java
@@ -0,0 +1,10 @@
+package com.example.justagram;
+
+public class Post {
+    public String mediaType;
+    public String mediaUrl;
+    public String caption;
+    public int likeCount;
+    public int commentsCount;
+    
+}
